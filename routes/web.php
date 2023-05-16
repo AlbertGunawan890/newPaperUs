8aca8cbb7aee5537b2d090455280d2881159f3da
AlbertGunawan890 <117983382+AlbertGunawan890@users.noreply.github.com>
2023-05-16 10:41:26 +0700

    master customer

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::post('/doAddbox', [BoxController::class, "doAdd"]);
 
 Route::get('/mastercustomer', [CustomerController::class, "show"]);
 Route::post('/doAddcustomer', [CustomerController::class, "doAdd"]);
+Route::post('/mastercustomer/delete/{id}', [CustomerController::class, "delete"]);
 
 Route::get('/mastersupplier', [SupplierController::class, "show"]);
 Route::post('/doAddsupplier', [SupplierController::class, "doAdd"]);
