0c4375338ebab085b0b3eec249342c891ecd8638
KevinOwen21 <kevinowen05@gmail.com>
2023-05-30 13:57:30 +0700

    Update autocomplete.php

--- public/autocomplete.php
+++ public/autocomplete.php
@@ -1,4 +1,7 @@
  <?php
+
+use Symfony\Component\Console\Logger\ConsoleLogger;
+
 $connect = mysqli_connect("localhost", "root", "", "db_paperus");
 
 if ($_REQUEST["ctr"] == "Penawaran") {
@@ -113,33 +116,34 @@ else if ($_REQUEST["ctr"] == "Processing1SPK") {
                     <tr>
                         <td>' . $ctr . '</td>
                         <td><input type="text" class="form-control" placeholder="Pilih proses"
-                        value="'.$row["jenis_proses"].'" name="proses['.$row["no_spk"].']" readonly></td>
+                        value="'.$row["jenis_proses"].'" name="proses['.$row["id"].']" readonly></td>
                         <td><select data-live-search="true" class=" form-control" id="id_vendor"
-                        name="nama_vendor['.$row["no_spk"].']">
-                        <option selected>---Pilih Vendor---</option>';
+                        name="nama_vendor['.$row["id"].']">';
                         $resultVendor = mysqli_query($connect, $queryVendor);
                         while ($rowVendor = mysqli_fetch_array($resultVendor)){
-                            $output .= '<option value="'. $rowVendor["nama_vendor"] .'">'. $rowVendor["nama_vendor"] .'</option>';
+                            $output .= '<option value="'. $rowVendor["nama_vendor"] .'"';
+                            if($rowVendor["nama_vendor"] == $row["nama_vendor"]) $output .= ' selected ';
+                            $output .= '>'. $rowVendor["nama_vendor"] .'</option>';
                         }
                 $output .= '</select></td>
                                     <td><input type="number" class="form-control" placeholder="Jumlah"
-                                    name="jumlah['. $row["no_spk"] .']" value="' . $row["jumlah"] . '"
-                                    onchange="harga_total_change('.$row["no_spk"].')">
+                                    name="jumlah['. $row["id"] .']" value="' . $row["jumlah"] . '"
+                                    onchange="harga_total_change('.$row["id"].')">
                             </td>
                             <td><input type="number" class="form-control" placeholder="Harga satuan"
-                                    name="harga_satuan['. $row["no_spk"].']" value="' . $row["harga_satuan"] . '"
-                                    onchange="harga_total_change('. $row["no_spk"].')"></td>
+                                    name="harga_satuan['. $row["id"].']" value="' . $row["harga_satuan"] . '"
+                                    onchange="harga_total_change('. $row["id"].')"></td>
                             <td>
                                 <input readonly type="number" class="form-control"
-                                    name="harga_total['. $row["no_spk"].']" value="'. $row["harga_total"] .'">
+                                    name="harga_total['. $row["id"].']" value="'. $row["harga_total"] .'">
                             </td>
                             <td><input readonly type="number" class="form-control"
-                                    name="harga_satuan_sebelumnya['. $row["no_spk"].']" value="'. $row["harga_satuan_sebelumnya"] .'"></td>
+                                    name="harga_satuan_sebelumnya['. $row["id"].']" value="'. $row["harga_satuan_sebelumnya"] .'"></td>
                             <td><input readonly type="number" class="form-control"
-                                    name="harga_total_sebelumnya['. $row["no_spk"].']" value="'. $row["harga_total_sebelumnya"] .'"></td>
+                                    name="harga_total_sebelumnya['. $row["id"].']" value="'. $row["harga_total_sebelumnya"] .'"></td>
                             <td>
                                 <button type="button" class="btn btn-success"
-                                    onclick="btnAcc('. $row["no_spk"].')"><i class="fas fa-check"></i></button>
+                                    onclick="btnAcc('. $row["id"].')"><i class="fas fa-check"></i></button>
                                 <button type="button" class="btn btn-danger"><i class="fas fa-times"></i></i></button>
                             </td>
                             <td>
@@ -155,17 +159,21 @@ else if ($_REQUEST["ctr"] == "Processing1SPK") {
 }
 else if ($_REQUEST["ctr"] == "AccSPKProcess1") {
     if (isset($_POST["query"])) {
-        $id_proses=$_POST['query'][0];
+        $id=$_POST['query'][0];
+        // dd($id);
+        $res_sebelumnya = mysqli_fetch_array(mysqli_query($connect, "SELECT * FROM `spk_processing1` WHERE `id` = '$id'"));
         $jenis_proses=$_POST['query'][1];
         $nama_vendor=$_POST['query'][2];
         $jumlah=$_POST['query'][3];
         $harga_satuan=$_POST['query'][4];
         $harga_total=$_POST['query'][5];
-        $harga_satuan_sebelumnya=$_POST['query'][6];
-        $harga_total_sebelumnya=$_POST['query'][7];
+        $harga_satuan_sebelumnya=$res_sebelumnya['harga_satuan'];
+        $harga_total_sebelumnya=$res_sebelumnya['harga_total'];
         $no_spk=$_POST['query'][8];
-        $query =  "INSERT INTO `spk_processing1`(`id`,`id_proses` ,`jenis_proses`,`nama_vendor`, `jumlah`, `harga_satuan`,`harga_total`,`harga_satuan_sebelumnya`, `harga_total_sebelumnya`, `status`,`no_spk`)
-        VALUES ('','$id_proses','$jenis_proses','$nama_vendor','$jumlah','$harga_satuan','$harga_total','$harga_satuan_sebelumnya','$harga_total_sebelumnya',1,'$no_spk')";
+        // $query =  "INSERT INTO `spk_processing1`(`id`,`id_proses` ,`jenis_proses`,`nama_vendor`, `jumlah`, `harga_satuan`,`harga_total`,`harga_satuan_sebelumnya`, `harga_total_sebelumnya`, `status`,`no_spk`)
+        // VALUES ('','$id_proses','$jenis_proses','$nama_vendor','$jumlah','$harga_satuan','$harga_total','$harga_satuan_sebelumnya','$harga_total_sebelumnya',1,'$no_spk')";
+        $query = "UPDATE `spk_processing1` SET `jenis_proses` = '$jenis_proses', `nama_vendor` = '$nama_vendor', `jumlah` = '$jumlah', `harga_satuan` = '$harga_satuan',
+        `harga_total` = '$harga_total', `harga_satuan_sebelumnya` = '$harga_satuan_sebelumnya', `harga_total_sebelumnya` = '$harga_total_sebelumnya' WHERE `id` = '$id'";
         $result = mysqli_query($connect, $query);
     }
 }
